c50498890ba529edb743b25662498174daf54291
Dimitri Capetz <dimitri.capetz@gmail.com>
2020-05-14 11:22:29 -0500

    Added logic to SSL verification

--- peer_interface_enabler.py
+++ peer_interface_enabler.py
@@ -84,7 +84,14 @@ import time
 
 # Set to allow unverified cert for eAPI call
 import ssl
-ssl._create_default_https_context = ssl._create_unverified_context
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    # Legacy Python that doesn't verify HTTPS certificates by default
+    pass
+else:
+    # Handle target environment that doesn't support HTTPS verification
+    ssl._create_default_https_context = _create_unverified_https_context
 
 # ----------------------------------------------------------------
 # Configuration section
